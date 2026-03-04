f2b2c36eae66e4e40cb166b0f498c42e0a3c1532
gabrielchboff <gabrielchagasboff@gmail.com>
2026-03-04 16:57:51 -0300

    FEATURE: First commit

--- server.js
+++ server.js
@@ -6,14 +6,23 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function(req, res){
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
