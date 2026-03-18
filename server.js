6262d30a0baae6b1f7567cfceda286d97ec1d8b7
gabrielchboff <gabrielchagasboff@gmail.com>
2026-03-18 16:43:26 -0300

    feat: New addition to the nav and error handling

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const env = require("dotenv").config();
 const path = require("path");
 const app = express();
 const static = require("./routes/static");
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -23,9 +26,30 @@ app.use(expressLayouts);
 app.use(static);
 
 // Index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" });
-});
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 /* ***********************
  * Local Server Information
